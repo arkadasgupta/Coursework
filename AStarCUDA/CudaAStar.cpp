c0c1e594adc01cd07917b5a393be42e7bc5d0b69
arka <arka@high-hopes>
2012-12-02 12:06:05 -0600

    C sequential code done

--- AStarCUDA/CudaAStar.cpp
+++ AStarCUDA/CudaAStar.cpp
@@ -12,6 +12,93 @@
 
 using namespace std;
 
+/*******************CUSTOM QUEUE**************/
+typedef struct heapData {
+    int nodeNum;
+    int parent;
+} heapData;
+ 
+typedef struct heapNode {
+    float value;
+    Node data;               //dummy
+} heapNode;
+ 
+typedef struct PQ {
+    heapNode* heap;
+    int size;
+} PQ;
+ 
+void insert(heapNode aNode, heapNode* heap, int size) {
+    int idx;
+    heapNode tmp;
+    idx = size + 1;
+    heap[idx] = aNode;
+    while (heap[idx].value < heap[idx/2].value && idx > 1) {
+    tmp = heap[idx];
+    heap[idx] = heap[idx/2];
+    heap[idx/2] = tmp;
+    idx /= 2;
+    }
+}
+ 
+void shiftdown(heapNode* heap, int size, int idx) {
+    int cidx;        //index for child
+    heapNode tmp;
+    for (;;) {
+        cidx = idx*2;
+        if (cidx > size) {
+            break;   //it has no child
+        }
+        if (cidx < size) {
+            if (heap[cidx].value > heap[cidx+1].value) {
+                ++cidx;
+            }
+        }
+        //swap if necessary
+        if (heap[cidx].value < heap[idx].value) {
+            tmp = heap[cidx];
+            heap[cidx] = heap[idx];
+            heap[idx] = tmp;
+            idx = cidx;
+        } else {
+            break;
+        }
+    }
+}
+ 
+heapNode removeMin(heapNode* heap, int size) {
+    //int cidx;
+    heapNode rv = heap[1];
+    //printf("%d:%d:%d\n", size, heap[1].value, heap[size].value);
+    heap[1] = heap[size];
+    --size;
+    shiftdown(heap, size, 1);
+    return rv;
+}
+void pq_enqueue(heapNode node, PQ *q) {
+    insert(node, q->heap, q->size);
+    ++q->size;
+}
+ 
+heapNode pq_dequeue(PQ *q) {
+   heapNode rv = removeMin(q->heap, q->size);
+   --q->size;
+   return rv; 
+}
+heapNode pq_peek(PQ *q) {
+   heapNode rv = q->heap[1];
+   //--q->size;
+   return rv; 
+} 
+void pq_initQueue(PQ *q, int n) {
+   q->size = 0;
+   q->heap = (heapNode*)malloc(sizeof(heapNode)*(n+1));
+}
+
+int pq_empty(PQ *q){
+    return q->size;
+}
+/*******************CUSTOM QUEUE**************/
 
 void findByAStar(GraphData *graph, int src_vertex, int from_vertex);
 void expand(Node **currentNode, GraphData *graph, float goalLat, float goalLong,
@@ -30,7 +117,7 @@ int main(int argc, char** argv) {
     cout << "Vertex Count:" << graph.vertexCount << endl;
     cout << "Edge Count:" << graph.edgeCount << endl;
 
-    //getNodeDetails(&graph,2);
+    //getNodeDetails(&graph,1363);
 
 
 
@@ -85,23 +172,77 @@ void findByAStar(GraphData *graph, int src_vertex, int goal_vertex) {
     float *fCostArray = (float*) malloc(graph->vertexCount * sizeof (float));
     memset(fCostArray, 0, graph->vertexCount * sizeof (float));
 
-    pqueue_t *frontier;
-    frontier = pqueue_init(graph->vertexCount, cmp_fCost, get_fCost, set_fCost, get_pos, set_pos);
-    //cout << "coming here" << endl;
+    /*********** APACHE QUEUE**************/
+//    pqueue_t *frontier;
+//    frontier = pqueue_init(graph->vertexCount, cmp_fCost, get_fCost, set_fCost, get_pos, set_pos);
+//    //cout << "coming here" << endl;
+//    Node src_node = {src_vertex, 0, 0};
+//    //Node goal_node = {goal_vertex,0,-1};
+//    float goalLat = graph->latitudes[goal_vertex - 1];
+//    float goalLong = graph->longitudes[goal_vertex - 1];
+//    pqueue_insert(frontier, &src_node);
+//    Node* currentNode;
+//    while (pqueue_peek(frontier) != NULL) {
+//        currentNode = (Node*) pqueue_pop(frontier);
+//        //cout<<"Vertex:"<<currentNode->nodeNum<<endl;
+//        if (currentNode->nodeNum == goal_vertex) {
+//            cout << "Solution found" << endl;
+//            return;
+//        } else {
+//            if (!(*(visitedArray + currentNode->nodeNum - 1))) {
+//                //                expand(&currentNode,graph,goalLat,goalLong,&frontier,
+//                //                        &gCostArray,&hCostArray,&fCostArray);
+//                int earliest_edge = graph->vertexArray[currentNode->nodeNum - 1];
+//                int final_edge = graph->vertexArray[currentNode->nodeNum];
+//                for (int i = earliest_edge; i < final_edge; i++) {
+//                    int child = graph->edgeArray[i];
+//                    if (child == currentNode->parent) {
+//                        continue;
+//                    }
+//                    //childNode.gCost = currentNode.gCost+e.weight;
+//                    //*(*(gCostArray) + child - 1) = *(*(gCostArray) + ((*(currentNode))->nodeNum - 1)) + graph->weightArray[i];
+//                    *(gCostArray + child - 1) = *(gCostArray+currentNode->nodeNum-1)+ graph->weightArray[i];
+//                    //cout<<"gcost:"<<*(gCostArray + child - 1);
+//                    float latitude = graph->latitudes[child - 1];
+//                    float longitude = graph->longitudes[child - 1];
+//                    //childNode.hCost=Graph.approxDist(latitude, longitude, goalNode.latitude, goalNode.longitude);
+//                    *(hCostArray + child - 1) = computeHeuristic(latitude, longitude, goalLat, goalLong);
+//                    //cout<<"hcost:"<<*(hCostArray + child - 1);
+//                    //childNode.fCost=childNode.gCost+childNode.hCost;
+//                    *(fCostArray + child - 1) = *(gCostArray + child - 1) + *(hCostArray + child - 1);
+//                    //cout<<"fcost:"<<*(fCostArray + child - 1);
+//                    //childNode.parent=currentNode;
+//                    Node childNode = {child, (*(fCostArray + child - 1)), currentNode->nodeNum};
+//                    //cout << "childNode info:" << childNode.nodeNum << ":" << childNode.fCost << ":" << childNode.parent << endl;
+//                    pqueue_insert(frontier, (void*) (&childNode));
+//                    Node* test = (Node*)pqueue_peek(frontier);
+//                    cout<<"after insert"<<test->nodeNum<<"--"<<test->fCost<<endl;
+//                
+//                }
+//                *(visitedArray + currentNode->nodeNum - 1) = true;
+//            } else {
+//                continue;
+//            }
+//        }
+//    }
+    /*********** APACHE QUEUE**************/
+    
+    /**************CUSTOM QUEUE*******/
+    PQ frontier;
+    pq_initQueue(&frontier, graph->vertexCount);
     Node src_node = {src_vertex, 0, 0};
+    heapNode src = {0,src_node};
     //Node goal_node = {goal_vertex,0,-1};
     float goalLat = graph->latitudes[goal_vertex - 1];
     float goalLong = graph->longitudes[goal_vertex - 1];
-    pqueue_insert(frontier, &src_node);
-    Node* currentNode;
-    //currentNode = (Node*)pq_dequeue(frontier,&pq_stat);
-    //cout<<"Vertex:"<<currentNode->nodeNum<<endl;
-
-    while (pqueue_peek(frontier) != NULL) {
-        Node* test = (Node*)pqueue_peek(frontier);
-        cout<<"at top"<<test->nodeNum<<endl;
-        currentNode = (Node*) pqueue_pop(frontier);
-        //cout<<"Vertex:"<<currentNode->nodeNum<<endl;
+    pq_enqueue(src, &frontier);
+    cout<<"froniter not emptyt"<<endl;
+    heapNode currentHNode;
+    while (pq_empty(&frontier)) {
+        //cout<<"froniter not emptyt"<<endl;
+        currentHNode = pq_dequeue(&frontier);
+        Node* currentNode = &(currentHNode.data);
+        //cout<<"node output:"<<currentNode->nodeNum<<endl;
         if (currentNode->nodeNum == goal_vertex) {
             cout << "Solution found" << endl;
             return;
@@ -119,16 +260,24 @@ void findByAStar(GraphData *graph, int src_vertex, int goal_vertex) {
                     //childNode.gCost = currentNode.gCost+e.weight;
                     //*(*(gCostArray) + child - 1) = *(*(gCostArray) + ((*(currentNode))->nodeNum - 1)) + graph->weightArray[i];
                     *(gCostArray + child - 1) = *(gCostArray+currentNode->nodeNum-1)+ graph->weightArray[i];
+                    //cout<<"gcost:"<<*(gCostArray + child - 1);
                     float latitude = graph->latitudes[child - 1];
                     float longitude = graph->longitudes[child - 1];
                     //childNode.hCost=Graph.approxDist(latitude, longitude, goalNode.latitude, goalNode.longitude);
                     *(hCostArray + child - 1) = computeHeuristic(latitude, longitude, goalLat, goalLong);
+                    //cout<<"hcost:"<<*(hCostArray + child - 1);
                     //childNode.fCost=childNode.gCost+childNode.hCost;
                     *(fCostArray + child - 1) = *(gCostArray + child - 1) + *(hCostArray + child - 1);
+                    //cout<<"fcost:"<<*(fCostArray + child - 1);
                     //childNode.parent=currentNode;
-                    Node childNode = {child, *(fCostArray + child - 1), currentNode->nodeNum};
+                    Node childNode = {child, (*(fCostArray + child - 1)), currentNode->nodeNum};
                     //cout << "childNode info:" << childNode.nodeNum << ":" << childNode.fCost << ":" << childNode.parent << endl;
-                    pqueue_insert(frontier, (void*) (&childNode));
+                    //pqueue_insert(frontier, (void*) (&childNode));
+                    heapNode childHNode = {(*(fCostArray + child - 1)),childNode};
+                    pq_enqueue(childHNode, &frontier);
+                    //Node* test = (Node*)pqueue_peek(frontier);
+                    //cout<<"after insert"<<test->nodeNum<<"--"<<test->fCost<<endl;
+                
                 }
                 *(visitedArray + currentNode->nodeNum - 1) = true;
             } else {
@@ -136,6 +285,8 @@ void findByAStar(GraphData *graph, int src_vertex, int goal_vertex) {
             }
         }
     }
+    /**************CUSTOM QUEUE*******/
+    
     free((void*) visitedArray);
     free((void*) gCostArray);
     free((void*) hCostArray);
---
+++ AStarCUDA/customQueue.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   customQueue.h
+ * Author: arka
+ *
+ * Created on December 2, 2012, 12:04 PM
+ */
+
+#ifndef CUSTOMQUEUE_H
+#define	CUSTOMQUEUE_H
+
+
+
+#endif	/* CUSTOMQUEUE_H */
+
