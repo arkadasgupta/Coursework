6c558cb9debfb8e78f29fa728d66e1f452277b5d
arka <arka@high-hopes>
2012-12-01 23:52:11 -0600

    segfault in pq.cpp

--- AStarCUDA/newmain.cpp
+++ AStarCUDA/newmain.cpp
@@ -37,7 +37,9 @@ int main(int argc, char** argv) {
     pq_t frontier = pq_new_queue(5,&compareNodeCost,&pq_stat);
     pq_enqueue(frontier,(void*)(&src_node),&pq_stat);
     pq_enqueue(frontier,(void*)(&goal_node),&pq_stat);
-    Node* currentNode = (Node*)pq_inspect_next(frontier,&pq_stat);
+    Node* currentNode = (Node*)pq_inspect_next(frontier,&pq_stat); //WORKING
+    cout<<currentNode->nodeNum<<endl;
+    Node* currentNode = (Node*)pq_dequeue(frontier,&pq_stat); //GIVING SEGFAULT FROM INSIDE THIS
     cout<<currentNode->nodeNum<<endl;
     return 0;
 }
